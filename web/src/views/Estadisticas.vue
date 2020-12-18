7bf518a52d90f42a4100fd130feb92d62c91b62c
Cabot, Juan Andres <juaancabot@gmail.com>
2020-12-18 00:41:23 -0300

    agregado paquete v-charts

--- web/src/views/Estadisticas.vue
+++ web/src/views/Estadisticas.vue
@@ -4,10 +4,59 @@
     <b-container fluid class="section-servicio2">
           <b-row class="justify-content-center">
           <b-col md="6" class="justify-items-center">
-            <h6 class="text-center m-4">Próximamente disponibles</h6>
-            <b-img :src="require('@/assets/clip-covid-19-spread-statistic.png')" fluid-grow></b-img>
+            <h2 class="text-center m-4">Rótulo Gráfico 1</h2>
+            <ve-pie :data="chartData" :settings="chartSettings" :id="Grafico1"></ve-pie>
+            <hr>
           </b-col>
-                    </b-row>
+          <b-col md="6" class="justify-items-center">
+            <h2 class="text-center m-4">Rótulo Gráfico 2</h2>
+            <ve-pie :data="chartData" :settings="chartSettings" :id="Grafico1"></ve-pie>
+            <hr>
+          </b-col>
+          </b-row>
+          <b-row>
+          <b-col md="6" class="justify-items-center">
+            <h2 class="text-center m-4">Rótulo Gráfico 3</h2>
+            <ve-pie :data="chartData" :settings="chartSettings" :id="Grafico1"></ve-pie>
+          </b-col>
+          <b-col md="6" class="justify-items-center">
+            <h2 class="text-center m-4">Rótulo Gráfico 4</h2>
+            <ve-pie :data="chartData" :settings="chartSettings" :id="Grafico1"></ve-pie>
+          </b-col>
+          </b-row>
     </b-container>
   </div>
 </template>
+<script>
+import VePie from 'v-charts/lib/pie.common'
+export default {
+  components: { VePie },
+  data () {
+    this.chartSettings = {
+      dimension: 'cost',
+      metrics: 'profit'
+    }
+    return {
+      chartData: {
+        columns: ['date', 'cost', 'profit'],
+        rows: [
+          { date: '01/01', cost: 123, profit: 3 },
+          { date: '01/02', cost: 1223, profit: 6 },
+          { date: '01/03', cost: 2123, profit: 90 },
+          { date: '01/04', cost: 4123, profit: 12 },
+          { date: '01/05', cost: 3123, profit: 15 },
+          { date: '01/06', cost: 7123, profit: 20 }
+        ]
+      }
+    }
+  }
+}
+
+/*
+Gráfico 1. Cantidad de turnos atendidos por centro. Torta.
+Gráfico 2. Gráfico por centro en un año, cantidad de turnos atendidos por mes. Gráfico en barras.
+Gráfico 3. Cantidad de centros por municipio. Torta.
+Gráfico 4(juan). Tipos de centros en base a total. Torta supongo.
+*/
+
+</script>

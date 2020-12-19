dee4bb849f8ceb9c740524c5e3981bfcfcaf487c
Laura Sofia Scotto <sofia.scotto@gmail.com>
2020-12-19 18:55:19 -0300

    grafico tipos de centros

--- web/src/views/Estadisticas.vue
+++ web/src/views/Estadisticas.vue
@@ -6,18 +6,10 @@
           <b-col md="6" class="justify-items-center">
             <h2 class="text-center m-4">Centros por municipio</h2>
             <ve-pie :data="chartData1" :settings="chartSettings1"></ve-pie>
-            <hr>
           </b-col>
           <b-col md="6" class="justify-items-center">
             <h2 class="text-center m-4">Turnos por Centro</h2>
             <ve-pie :data="chartDataTurnos" :settings="chartSettingsTurnos"></ve-pie>
-            <hr>
-          </b-col>
-          </b-row>
-          <b-row>
-          <b-col md="6" class="justify-items-center">
-            <h2 class="text-center m-4">Rótulo Gráfico 3</h2>
-            <ve-pie :data="chartData" :settings="chartSettings"></ve-pie>
           </b-col>
           <b-col md="6" class="justify-items-center">
             <h2 class="text-center m-4">Tipos de Centros</h2>
@@ -60,17 +52,6 @@ export default {
         columns: ['town', 'countCenter'],
         rows: []
       },
-      chartData: {
-        columns: ['date', 'cost', 'profit'],
-        rows: [
-          { date: '01/01', cost: 123, profit: 3 },
-          { date: '01/02', cost: 1223, profit: 6 },
-          { date: '01/03', cost: 2123, profit: 90 },
-          { date: '01/04', cost: 4123, profit: 12 },
-          { date: '01/05', cost: 3123, profit: 15 },
-          { date: '01/06', cost: 7123, profit: 20 }
-        ]
-      },
       chartDataCenterType: {
         columns: ['centerType', 'countCenterType'],
         rows: []
@@ -111,12 +92,5 @@ export default {
       })
   }
 }
-
-/*
-Gráfico 1. Cantidad de turnos atendidos por centro. Torta.
-Gráfico 2. Gráfico por centro en un año, cantidad de turnos atendidos por mes. Gráfico en barras.
-Gráfico 3. Cantidad de centros por municipio. Torta.
-Gráfico 4(juan). Tipos de centros en base a total. Torta supongo.
-*/
 
 </script>
